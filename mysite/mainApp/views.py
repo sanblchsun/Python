5fae412a4e9cd6897f40821eb4939924fbac0657
sanblchsun <logovopost@yandex.ru>
2018-07-31 00:00:32 +0300

    extension of the application "mainApp" to display the Contacts page

--- mysite/mainApp/views.py
+++ mysite/mainApp/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values': ['Если у вас'
+    ' остались вопросы , то задавайте их мне по телефону','(086) 086-86-86',
+    'example@post.com']})
